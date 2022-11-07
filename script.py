9f0064f804f741811ffdf9cf720d83e5e632ddae
korneliuszburian <krnijuu@gmail.com>
2022-11-07 23:03:46 +0100

    Removed duplicated ricetory

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_filtered_links(url, filter):
-    
+    """Returns a list of links that match a filter"""
     links = []
     
     r = requests.get(url)
@@ -22,13 +22,13 @@ def get_filtered_links(url, filter):
     return correct_link
 
 def extract_zip_file(zip_file, filter, destination):
-
+    """Extracts a zip file to a destination folder"""
     with zipfile.ZipFile(zip_file[0], 'r') as zip_ref:
         for y in zip_ref.namelist():
             if y.startswith(filter):
                 zip_ref.extractall(destination)
                 os.startfile(destination + y)
         
-        
+output_path = 'C:/Users/username/Desktop/'
 retrieved_file = urllib.request.urlretrieve(get_filtered_links(('http://leagueskin.net/p/download-mod-skin-2020-chn'), '.zip'))
-extract_zip_file(retrieved_file, 'LOLPRO', 'I:\\programming\\league-skin-downloader\\test_folder\\')
+extract_zip_file(retrieved_file, 'LOLPRO', output_path)
